9b72e65b7f513ef430a6c6491a14492cd0c5c18f
unknown <dencooler@gmail.com>
2020-12-29 22:14:49 +0300

    add: blizz auth

--- app/api/v1/handler.go
+++ app/api/v1/handler.go
@@ -1,18 +1,60 @@
 package v1
 
 import (
-	logging "auctioneer/app/logger"
+	"auctioneer/app/conf"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 type Handler interface {
+	MakeBlizzAuth() error
 }
 
 type v1Handler struct {
-	log *logging.Logger
+	token       *BlizzardToken
+	BlizzApiCfg *conf.BlizzApiCfg
+	// log      *logging.Logger
 }
 
-func NewBasehandlerv1(log *logging.Logger) Handler {
+func NewBasehandlerv1(blizzCfg *conf.BlizzApiCfg) Handler {
 	return &v1Handler{
-		log: log,
+		BlizzApiCfg: blizzCfg,
 	}
+
+}
+
+func (h *v1Handler) MakeBlizzAuth() error {
+	body := strings.NewReader("grant_type=client_credentials")
+
+	request, err := http.NewRequest(http.MethodPost, h.BlizzApiCfg.AUTHUrl.String(), body)
+	if err != nil {
+		return fmt.Errorf("Error creating request: %v", err)
+	}
+	request.SetBasicAuth(h.BlizzApiCfg.ClientID, h.BlizzApiCfg.ClientSecret)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	httpClient := new(http.Client)
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return fmt.Errorf("Error making blizzard auth request: %v", err)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return fmt.Errorf("Error reading blizzard auth response: %v", err)
+	}
+
+	tokenData := new(BlizzardToken)
+	err = json.Unmarshal(responseData, &tokenData)
+	if err != nil {
+		return fmt.Errorf("Error unmarshaling blizzard auth response: %v", err)
+	}
+
+	h.token = tokenData
+
+	return nil
 }
