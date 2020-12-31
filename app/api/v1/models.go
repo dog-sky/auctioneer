6b2db367e713b182fad77e98d6bf7677b5246403
unknown <dencooler@gmail.com>
2020-12-31 14:25:56 +0300

    add: get realmist on startup

--- app/api/v1/models.go
+++ app/api/v1/models.go
@@ -10,3 +10,21 @@ type BlizzardToken struct {
 	TokenType   string `json:"token_type"`
 	ExpiresIn   int    `json:"expires_in"`
 }
+
+type Realms struct {
+	Key struct {
+		Href string `json:"href"`
+	} `json:"key"`
+	Name string `json:"name"`
+	ID   int    `json:"id"`
+	Slug string `json:"slug"`
+}
+
+type BlizzRealmsSearchResult struct {
+	Links struct {
+		Self struct {
+			Href string `json:"href"`
+		} `json:"self"`
+	} `json:"_links"`
+	Realms []Realms `json:"realms"`
+}
