5d4d21f34f7b811729403db545fa3e06fb80eebd
unknown <dencooler@gmail.com>
2021-01-02 15:10:53 +0300

    add: search auction house

--- app/api/v1/auctioneer.go
+++ app/api/v1/auctioneer.go
@@ -1,36 +1,54 @@
 package v1
 
 import (
+	"auctioneer/app/blizz"
 	"fmt"
 	fiber "github.com/gofiber/fiber/v2"
 )
 
 func (h *V1Handler) SearchItemData(c *fiber.Ctx) error {
-	queryParams := new(searchQueryParams)
-	if err := c.QueryParser(queryParams); err != nil {
+	params := new(searchQueryParams)
+	if err := c.QueryParser(params); err != nil {
 		return fiber.NewError(
 			fiber.StatusBadRequest,
 			fmt.Sprintf("Invalid query params. Err: %v", err),
 		)
 	}
 
-	if err := checkQueryParams(queryParams); err != nil {
+	if err := checkQueryParams(params); err != nil {
 		return err
 	}
 
-	realmID := h.BlizzClient.GetRealmID(queryParams.RealmName)
+	realmID := h.BlizzClient.GetRealmID(params.RealmName)
 	if realmID == 0 {
 		return fiber.NewError(
 			fiber.StatusNotFound,
-			fmt.Sprintf("Realm %s not found", queryParams.RealmName),
+			fmt.Sprintf("Realm %s not found", params.RealmName),
 		)
 	}
 
 	res := new(ResponseV1)
 	res.Success = true
 
-	// itemData
-	_, err := h.BlizzClient.SearchItem(queryParams.ItemName, queryParams.Region)
+	itemData := new(blizz.ItemResult)
+	if params.ItemName != "" {
+		searchResult, err := h.BlizzClient.SearchItem(params.ItemName, params.Region)
+		if err != nil {
+			return fiber.NewError(
+				fiber.StatusBadRequest,
+				err.Error(),
+			)
+		}
+		if len(searchResult.Results) == 0 {
+			return fiber.NewError(
+				fiber.StatusNotFound,
+				fmt.Sprintf("Item %s not found", params.ItemName),
+			)
+		}
+		itemData = searchResult
+	}
+
+	data, err := h.BlizzClient.GetAuctionData(realmID, params.Region)
 	if err != nil {
 		return fiber.NewError(
 			fiber.StatusBadRequest,
@@ -38,8 +56,20 @@ func (h *V1Handler) SearchItemData(c *fiber.Ctx) error {
 		)
 	}
 
-	// сделать запрос в аук по серверу
-	// отдать в ответе только то, что пересекается с запрошеным предметом (или всё, если запрос пустой)
+	if len(itemData.Results) == 0 {
+		res.Result = data
+		return c.JSON(res)
+	}
 
+	for _, AucItem := range data {
+		for _, item := range itemData.Results {
+			if AucItem.Item.ID == item.Data.ID {
+				res.Result = append(res.Result, AucItem)
+			}
+		}
+	}
+
+	// TODO В ответ в модели нужно ещё выдавать имя предмета и его качество.
+	// возможно нам нужна функциональность только поиска по предмету и не делать функциональность выгрузки всего аука
 	return c.JSON(res)
 }
